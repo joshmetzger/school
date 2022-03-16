7706ef76b48eb456cec918ab37bc7309d7faf2ea
vagrant <vagrant@school>
2022-03-16 21:03:16 +0000

    user edit and update working ok

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,8 @@ Route::prefix('users')->group(function(){
     Route::get('/view', [UserController::class, 'UserView'])->name('user.view');
     Route::get('/add', [UserController::class, 'UserAdd'])->name('user.add');
     Route::post('/store', [UserController::class, 'UserStore'])->name('user.store');
+    Route::get('/edit/{id}', [UserController::class, 'UserEdit'])->name('user.edit');
+    Route::post('/update/{id}', [UserController::class, 'UserUpdate'])->name('user.update');
     
 
 });
